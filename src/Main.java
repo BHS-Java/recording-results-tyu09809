443005ac1fc0eebf183545f3f640e23edf79a1f7
Tanush <1117464@apps.nsd.org>
2025-01-30 15:15:38 -0800

    first assignment done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,38 @@
 import java.util.Scanner;
 public class Main implements Spec{
     public static void main(String[] args){
-        Player p1 = new Player();
-        p1.getName();
-        p1.getAge();
-        p1.getHeight();
+        System.out.println();
+        Main main = new Main();
+        Person player = main.makePerson("Name pls");
+        int age = main.askNumber("What age");
+        String height = main.askString("How tall");
     }
+
+    public String askString(String question){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(question);
+        String answer = scanner.nextLine();
+        return answer;
+    }
+
+    public int askNumber(String question){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(question);
+        String answer = scanner.nextLine();
+        int numberAnswer = Integer.parseInt(answer);
+        return numberAnswer;
+    }
+    public Results getResults(Game guessingOrBetter){
+        return getResults(guessingOrBetter);
+    }
+    public Person makePerson(String question){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(question);
+        String answer = scanner.nextLine();
+        return new Person(answer);
+    }
+    public void addResults(Person player){
+        return;
+    }
+
 }
--- src/Results.java
+++ src/Results.java
@@ -1,17 +1,1 @@
-public interface Results {
-    int wins = 0;
-    int losses = 0;
-    int ties = 0;
-    
-    public static int getWins() {
-        return wins;
-    }
-
-    public static int getLosses() {
-        return losses;
-    }
-
-    public static int getTies() {
-        return ties;
-    }
-}
+public interface Results {}
