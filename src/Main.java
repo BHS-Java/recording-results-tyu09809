25f431f6afe2db57b8e70d72b2017e910b410ca5
Tanush <86380850+tyu09809@users.noreply.github.com>
2025-01-29 18:30:35 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 public class Main implements Spec{
     public static void main(String[] args){
         Player p1 = new Player();
-        p1.getName();
-        p1.getAge();
-        p1.getHeight();
+        System.out.println("Player stuff");
+        System.out.println("Name: " + p1.getName());
+        System.out.println("Height: " + p1.getHeight());
+        System.out.println("Age: " + p1.getAge());
     }
 }
